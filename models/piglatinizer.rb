82e8fc6e43342611f860c267ec37292d80d4c8ac
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-07-14 19:30:22 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,20 +10,20 @@ class PigLatinizer
 
     def piglatinize_word(word)
         first_letter = word[0].downcase
-        if ["a", "e", "i", "o", "u"].include?(first_letter)
+        vowels = %w(a e i o u )
+        if vowels.include?(first_letter)
             "#{word}way"
         else
-            consonants = []
+            consonants = [ ]
             consonants << word[0]
-            if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+            if vowels.include?(word[1]) == false
                 consonants << word[1]
-                if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+                if vowels.include?(word[2]) == false
                     consonants << word[2]
                 end
             end
             "#{word[consonants.length..-1] + consonants.join + "ay"}"
         end
-        
     end
    
 

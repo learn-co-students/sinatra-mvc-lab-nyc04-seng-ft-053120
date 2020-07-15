c96381b2258b9eb9014bfe882b3cd86d4f6b2818
“DonatelloTHM” <donat.pllana@outlook.com>
2020-07-14 21:41:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/" do
+        erb :user_input
+    end
+
+    post "/piglatinize" do
+        piglatinize=PigLatinizer.new
+        @piglatized=piglatinize.transformed(params[:user_phrase])
+        erb :results
+    end
 end
